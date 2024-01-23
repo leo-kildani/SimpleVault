7e6bf27c91a7c81b26e0e812d1ab2e180740631c
Leo <leonardo.kildani@sjsu.edu>
2024-01-22 18:10:47 -0800

    modify security SQL scripts

--- src/main/resources/scripts/3_create_security_tables.sql
+++ src/main/resources/scripts/3_create_security_tables.sql
@@ -3,14 +3,15 @@ USE simplevault;
 CREATE TABLE client_logins (
     username VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL PRIMARY KEY,
     password CHAR(72) NOT NULL,
-    client_id INT NOT NULL,
+    client_id INT,
     enabled TINYINT NOT NULL,
     FOREIGN KEY (client_id) REFERENCES clients(client_id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE client_roles (
+    role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL,
     `role` VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL,
-    constraint fk_roles_client_logins FOREIGN KEY (username) REFERENCES client_logins(username)
+    constraint fk_roles_logins FOREIGN KEY (username) REFERENCES client_logins(username)
 );
-CREATE UNIQUE INDEX ix_role_username ON roles (username, `role`);
+CREATE UNIQUE INDEX ix_role_username ON client_roles (username, `role`);
--- src/main/resources/scripts/4_create_test_schema_and_tables.sql
+++ src/main/resources/scripts/4_create_test_schema_and_tables.sql
@@ -54,3 +54,19 @@ CREATE TABLE transactions (
     FOREIGN KEY (source_account_id) REFERENCES accounts(account_id),
     FOREIGN KEY (destination_account_id) REFERENCES accounts(account_id)
 );
+
+CREATE TABLE client_logins (
+    username VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL PRIMARY KEY,
+    password CHAR(72) NOT NULL,
+    client_id INT,
+    enabled TINYINT NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clients(client_id)
+);
+
+CREATE TABLE client_roles (
+    role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL,
+    `role` VARCHAR(64) COLLATE utf8mb4_general_ci NOT NULL,
+    constraint fk_roles_logins FOREIGN KEY (username) REFERENCES client_logins(username)
+);
+CREATE UNIQUE INDEX ix_role_username ON client_roles (username, `role`);
